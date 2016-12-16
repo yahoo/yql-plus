476cfe461d11b1256ec2b4c9fb318a3bda060c75
Yi Zhu <daisyzhu@yahoo-inc.com>
2016-12-15 19:32:36 -0800

    add a script for yqlplus_language api

--- travis/deploy_yqlplus_language.sh
+++ travis/deploy_yqlplus_language.sh
@@ -5,8 +5,10 @@ set -e
 echo "TRAVIS_TAG is ${TRAVIS_TAG}" 
 
 test "${TRAVIS_PULL_REQUEST}" == "false"
+echo "not a PR buid"
 test "${TRAVIS_BRANCH}" == "master"
-test "${TRAVIS_TAG}" != ""
+echo "branch is master"
+
 cd yqlplus_language
 echo "inside yqlplus_language"
 pwd
