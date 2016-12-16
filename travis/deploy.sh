b430cc01f049ad788bfdb946b4724a915a45b233
Yi Zhu <daisyzhu@yahoo-inc.com>
2016-12-15 19:38:33 -0800

    enable bintray deploy
    enable bintray deploy

--- travis/deploy.sh
+++ travis/deploy.sh
@@ -6,5 +6,4 @@ echo "TRAVIS_TAG is ${TRAVIS_TAG}"
 
 test "${TRAVIS_PULL_REQUEST}" == "false"
 test "${TRAVIS_BRANCH}" == "master"
-test "${TRAVIS_TAG}" != ""
 mvn deploy --settings travis/settings.xml
