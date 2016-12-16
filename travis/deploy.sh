dd6921fb8ee63ff77c94f11cfea3cf3e5a996f43
Yi Zhu <daisyzhu@yahoo-inc.com>
2016-12-15 19:53:59 -0800

    build deploy script

--- travis/deploy.sh
+++ travis/deploy.sh
@@ -1,9 +1,26 @@
 #!/usr/bin/env bash
 
-set -e
-
-echo "TRAVIS_TAG is ${TRAVIS_TAG}" 
+set -ev
 
 test "${TRAVIS_PULL_REQUEST}" == "false"
 test "${TRAVIS_BRANCH}" == "master"
-mvn deploy --settings travis/settings.xml
+
+# Pushing yqlplus_language
+cd yqlplus_language
+mvn deploy --settings ../travis/settings.xml
+cd ..
+
+# Pushing yqlplus_source_api
+cd yqlplus_source_api
+mvn deploy --settings ../travis/settings.xml
+cd ..
+
+# Pushing yqlplus_engine
+cd yqlplus_engine
+mvn deploy --settings ../travis/settings.xml
+cd ..
+
+# Pushing yqlplus_stdlib
+cd yqlplus_stdlib
+mvn deploy --settings ../travis/settings.xml
+cd ..
