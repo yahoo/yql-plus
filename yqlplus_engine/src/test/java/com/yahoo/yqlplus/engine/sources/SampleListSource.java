3b08e95823e3fef6004a15948a321e159d26cd41
Yi Zhu <daisyzhu@yahoo-inc.com>
2017-11-30 21:48:46 -0800

    Fix Iterable assignable
    add fix for object
    
    improve

---
+++ yqlplus_engine/src/test/java/com/yahoo/yqlplus/engine/sources/SampleListSource.java
@@ -0,0 +1,31 @@
+/*
+ * Copyright (c) 2016 Yahoo Inc.
+ * Licensed under the terms of the Apache version 2.0 license.
+ * See LICENSE file for terms.
+ */
+
+package com.yahoo.yqlplus.engine.sources;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import com.yahoo.yqlplus.api.Source;
+import com.yahoo.yqlplus.api.annotations.Query;
+
+public class SampleListSource implements Source {
+
+    @Query
+    public Iterable<Sample> getSampleIds() throws InterruptedException {
+
+        Set<Sample> ids = new HashSet<>();
+        for (int i = 0; i < 1; i++) {
+            ids.add(new Sample("test", i));
+        }
+        return ids;
+    }
+    
+    @Query
+    public Iterable<Sample> getSampleIds(Iterable<Sample> ids) {
+      return ids;
+    }
+}
