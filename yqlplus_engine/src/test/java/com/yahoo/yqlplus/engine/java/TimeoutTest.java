6881b983e454ef5f89f3dd3296ccbfd3b520b8bc
Ken Fox <fox@xythian.com>
2018-07-14 15:00:12 -0700

    Factor Guice out of TimeoutTest.

--- yqlplus_engine/src/test/java/com/yahoo/yqlplus/engine/java/TimeoutTest.java
+++ yqlplus_engine/src/test/java/com/yahoo/yqlplus/engine/java/TimeoutTest.java
@@ -8,8 +8,6 @@ package com.yahoo.yqlplus.engine.java;
 
 import com.google.common.collect.ImmutableList;
 import com.google.common.collect.ImmutableMap;
-import com.google.inject.Guice;
-import com.google.inject.Injector;
 import com.yahoo.yqlplus.engine.CompiledProgram;
 import com.yahoo.yqlplus.engine.ProgramResult;
 import com.yahoo.yqlplus.engine.TaskContext;
@@ -25,12 +23,11 @@ import java.util.concurrent.ExecutionException;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.TimeoutException;
 
-public class TimeoutTest {
+public class TimeoutTest extends ProgramTestBase  {
 
     @Test(enabled = false)
     public void testTimeoutWorking() throws Exception {
-        Injector injector = Guice.createInjector(new JavaTestModule(), new SourceBindingModule("timers", TimeoutSource.class));
-        YQLPlusCompiler compiler = injector.getInstance(YQLPlusCompiler.class);
+        YQLPlusCompiler compiler = createCompiler("timers", TimeoutSource.class);
         CompiledProgram program = compiler.compile("SELECT * FROM timers(10) OUTPUT as f1;");
         ProgramResult rez = program.run(ImmutableMap.of());
         Assert.assertEquals(rez.getResult("f1").get().getResult(), ImmutableList.of(new Person("1", "1", 1)));
@@ -38,8 +35,7 @@ public class TimeoutTest {
 
     @Test(enabled = false)
     public void testTimeoutFailing() throws Exception {
-        Injector injector = Guice.createInjector(new JavaTestModule(), new SourceBindingModule("timers", TimeoutSource.class));
-        YQLPlusCompiler compiler = injector.getInstance(YQLPlusCompiler.class);
+        YQLPlusCompiler compiler = createCompiler("timers", TimeoutSource.class);
         CompiledProgram program = compiler.compile("SELECT * FROM timers(10) TIMEOUT 5 OUTPUT as f1;");
         try {
             List<Person> rez = program.run(ImmutableMap.of()).getResult("f1").get().getResult();
@@ -51,8 +47,7 @@ public class TimeoutTest {
 
     @Test(enabled = false)
     public void testTimeoutCheck() throws Exception {
-        Injector injector = Guice.createInjector(new JavaTestModule(), new SourceBindingModule("timers", TimeoutSource.class));
-        YQLPlusCompiler compiler = injector.getInstance(YQLPlusCompiler.class);
+        YQLPlusCompiler compiler = createCompiler("timers", TimeoutSource.class);
         CompiledProgram program = compiler.compile("SELECT * FROM timers(8, false) TIMEOUT 5 OUTPUT as f1;");
         try {
             List<Person> rez = program.run(ImmutableMap.of()).getResult("f1").get().getResult();
@@ -64,8 +59,7 @@ public class TimeoutTest {
 
     @Test(enabled = false)
     public void testTimeoutCheckWorks() throws Exception {
-        Injector injector = Guice.createInjector(new JavaTestModule(), new SourceBindingModule("timers", TimeoutSource.class));
-        YQLPlusCompiler compiler = injector.getInstance(YQLPlusCompiler.class);
+        YQLPlusCompiler compiler = createCompiler("timers", TimeoutSource.class);
         CompiledProgram program = compiler.compile("SELECT * FROM timers(5, true) TIMEOUT 5 OUTPUT as f1;");
         ProgramResult rez = program.run(ImmutableMap.of());
         Assert.assertEquals(rez.getResult("f1").get().getResult(), ImmutableList.of(new Person("1", "1", 1)));
@@ -74,15 +68,12 @@ public class TimeoutTest {
     @Test(enabled = false)
     public void testProgramTimeout() throws Exception {
         // Not overriding default program timeout
-        Injector injector = Guice.createInjector(new JavaTestModule(), new SourceBindingModule("timers", TimeoutSource.class));
-        YQLPlusCompiler compiler = injector.getInstance(YQLPlusCompiler.class);
+        YQLPlusCompiler compiler = createCompiler("timers", TimeoutSource.class);
         CompiledProgram program = compiler.compile("SELECT * FROM timers(20) OUTPUT as f1;");
         ProgramResult rez = program.run(ImmutableMap.of());
         Assert.assertEquals(rez.getResult("f1").get().getResult(), ImmutableList.of(new Person("1", "1", 1)));
         
         // Overriding default program timeout
-        injector = Guice.createInjector(new JavaTestModule(), new SourceBindingModule("timers", TimeoutSource.class));
-        compiler = injector.getInstance(YQLPlusCompiler.class);
         program = compiler.compile("SELECT * FROM timers(20) OUTPUT as f1;");
         try {
             program.run(ImmutableMap.of(), TaskContext.builder().withTimeout(5, TimeUnit.MILLISECONDS).build()).getResult("f1").get().getResult();
@@ -98,8 +89,7 @@ public class TimeoutTest {
 
     @Test(enabled = false)
     public void testTimeoutFailingJoin() throws Exception {
-        Injector injector = Guice.createInjector(new JavaTestModule(), new SourceBindingModule("timers", TimeoutSource.class));
-        YQLPlusCompiler compiler = injector.getInstance(YQLPlusCompiler.class);
+        YQLPlusCompiler compiler = createCompiler("timers", TimeoutSource.class);
         CompiledProgram program = compiler.compile("SELECT * FROM timers(10) t1 JOIN timers(10) t2 ON t1.id = t2.id TIMEOUT 5 OUTPUT as f1;");
         try {
             program.run(ImmutableMap.of()).getResult("f1").get().getResult();
@@ -111,8 +101,7 @@ public class TimeoutTest {
 
     @Test(enabled = false)
     public void testTimeoutCheckJoin() throws Exception {
-        Injector injector = Guice.createInjector(new JavaTestModule(), new SourceBindingModule("timers", TimeoutSource.class));
-        YQLPlusCompiler compiler = injector.getInstance(YQLPlusCompiler.class);
+        YQLPlusCompiler compiler = createCompiler("timers", TimeoutSource.class);
         CompiledProgram program = compiler.compile("SELECT * FROM timers(5, false) t1 JOIN timers(5, false) t2 ON t1.id = t2.id TIMEOUT 5 OUTPUT as f1;");
         try {
             program.run(ImmutableMap.of()).getResult("f1").get().getResult();
@@ -124,16 +113,14 @@ public class TimeoutTest {
 
     @Test(expectedExceptions = {ProgramCompileException.class}, expectedExceptionsMessageRegExp = "<string>:L1:24 no viable alternative at input 'SELECT \\* FROM timers\\(10 JOIN'")
     public void testParseException() throws Exception {
-        Injector injector = Guice.createInjector(new JavaTestModule(), new SourceBindingModule("timers", TimeoutSource.class));
-        YQLPlusCompiler compiler = injector.getInstance(YQLPlusCompiler.class);
+        YQLPlusCompiler compiler = createCompiler("timers", TimeoutSource.class);
         // bad syntax
         compiler.compile("SELECT * FROM timers(10 JOIN timers(10) t2 ON t1.id = t2.id TIMEOUT 5 OUTPUT as f1;");
     }
 
     @Test(expectedExceptions = {ProgramCompileException.class}, expectedExceptionsMessageRegExp = "<string>:L1:83 mismatched input '.' expecting ';'")
     public void testParseException2() throws Exception {
-        Injector injector = Guice.createInjector(new JavaTestModule(), new SourceBindingModule("timers", TimeoutSource.class));
-        YQLPlusCompiler compiler = injector.getInstance(YQLPlusCompiler.class);
+        YQLPlusCompiler compiler = createCompiler("timers", TimeoutSource.class);
         // bad syntax
         compiler.compile("SELECT * FROM timers(10) JOIN timers(10) t2 ON t1.id = t2.id TIMEOUT 5 OUTPUT as f1.bar;");
     }
