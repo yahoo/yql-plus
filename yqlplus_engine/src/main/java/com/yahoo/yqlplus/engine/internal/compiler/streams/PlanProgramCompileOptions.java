c5aaaa4eb5d7843407f57c176ee6138806067d51
Yi Zhu <dsyzhu@gmail.com>
2020-04-18 11:18:39 -0700

    add missing file

---
+++ yqlplus_engine/src/main/java/com/yahoo/yqlplus/engine/internal/compiler/streams/PlanProgramCompileOptions.java
@@ -0,0 +1,35 @@
+package com.yahoo.yqlplus.engine.internal.compiler.streams;
+
+/**
+ * Created by daisyzhu on 4/17/20.
+ */
+public class PlanProgramCompileOptions {
+    private boolean keepMergeSequential; //enable this option merge tables will be sequential
+
+    private PlanProgramCompileOptions(PlanProgramOptionsBuilder builder) {
+        this.keepMergeSequential = builder.keepMergeSequential;
+    }
+
+    public boolean isKeepMergeSequential() {
+        return keepMergeSequential;
+    }
+
+    public static final class PlanProgramOptionsBuilder {
+        private boolean keepMergeSequential;
+        public PlanProgramOptionsBuilder keepMergeSequential(boolean keepMergeSequential) {
+            this.keepMergeSequential = keepMergeSequential;
+            return this;
+        }
+
+        public PlanProgramCompileOptions build() {
+            return new PlanProgramCompileOptions(this);
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "PlanProgramCompileOptions{" +
+                "keepMergeSequential=" + keepMergeSequential +
+                '}';
+    }
+}
